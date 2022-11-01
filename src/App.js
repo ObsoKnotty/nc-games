d4e4d503d8626e42215be3a70e1eaa533ad8c701
ObsoKnotty <tkirk1331@gmail.com>
2022-11-01 10:20:52 +0000

    adds in route functionality and moves Reviews to a route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import Header from "./components/Header";
 import Reviews from "./components/Reviews";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Reviews />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/reviews" element={<Reviews />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
